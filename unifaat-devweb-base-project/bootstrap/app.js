3206d964b589c9f99e4b02fd7dfd795b837f531e
Plastycos <neverax123@gmail.com>
2025-06-24 02:32:57 -0300

    Mais mudanças

--- unifaat-devweb-base-project/bootstrap/app.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-import constants from '../config/constants.js';
-
-/** Init .env file */
-dotenv.config();
-
-/** Inserir as constantes na variavel global */
-globalThis.CONSTANTS = constants;
--- unifaat-devweb-base-project/docker/postgres/init/001-turma.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS turma (
-    id_turma SERIAL PRIMARY KEY,
-    nome_professor VARCHAR(255)
-);
-
-INSERT INTO turma (nome_professor) VALUES
-('Roberto'),
-('Josivaldo');
